7a19402fa2e86d056a7f64721bf6a08837f3a338
Kerstens Maxim <maxim.kerstens@gmail.com>
2025-07-20 23:09:56 +0200

    chore: update docblocks

--- src/Logger/Contracts/Logger.php
+++ src/Logger/Contracts/Logger.php
@@ -11,17 +11,25 @@ use Saloon\Http\Response;
 
 interface Logger
 {
+    /**
+     * Just before a request is sent
+     * Returns log data (null if none can be created)
+     */
     public function create(PendingRequest $request, Connector $connector): mixed;
 
     /**
-     * @param  mixed  $log  The log that was returned from $this->>create())
-     * @return mixed The log
+     * Right after a request was sent.
+     *
+     * @param mixed $log The log that was returned from $this->create()
+     * @return mixed The updated log
      */
     public function updateWithResponse(mixed $log, Response $response, Connector $connector): mixed;
 
     /**
-     * @param  mixed  $log  The log that was returned from $this->>create())
-     * @return mixed The log
+     * In case there was a fatal error (due to Saloon not being able to connect, for example).
+     *
+     * @param mixed $log The log that was returned from $this->create()
+     * @return mixed The updated log
      */
     public function updateWithFatalError(mixed $log, FatalRequestException $errorResponse, Connector $connector): mixed;
 }
